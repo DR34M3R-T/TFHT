e0379f2206ae1bb657f7acce09b3c597b478a57a
DreamerT <1731428261@qq.com>
2022-02-16 10:07:35 +0800

    添加注释及代码结构优化

--- ViT_torch_train_rotor.py
+++ ViT_torch_train_rotor.py
@@ -1,34 +1,26 @@
 import torch
 torch.set_default_tensor_type(torch.DoubleTensor)
-from torch import nn
 from torch.utils.data import DataLoader,Dataset
 from torchvision import transforms
-from vit_pytorch import Transformer
-from einops import repeat
-from einops.layers.torch import Rearrange
 import numpy as np
+import MyViT
 
 # 设定训练用的设备
 device = "cuda" if torch.cuda.is_available() else "cpu"
 # 打印看一下
 print("Using {} device".format(device))
 
-learning_rate = 0.0008
-epochs = 15
+# 导入raw数据集
 x_train = torch.from_numpy(np.load('./dataset/XJTU/xTrain.npy'))
 y_train = torch.from_numpy(np.load('./dataset/XJTU/yTrain.npy'))
 x_test = torch.from_numpy(np.load('./dataset/XJTU/xTest.npy'))
 y_test = torch.from_numpy(np.load('./dataset/XJTU/yTest.npy'))
 
-def pair(t):
-    return t if isinstance(t, tuple) else (t, t)
-
-# 自定义dataset和数据预处理
+# 自定义dataset和数据集预处理
 preprocess = transforms.Compose([
     transforms.ToTensor(),
     # transforms.Normalize(mean=0, std=1)
 ])
-
 class BearFaultDataset(Dataset):
     def __init__(self, inputs, targets, transform, reshape):
         inputs_f=torch.abs(torch.fft.fft(inputs))
@@ -65,51 +57,7 @@ batch_size = 64
 train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
-#ViT
-class myViT(nn.Module):
-    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 2, dim_head = 64, dropout = 0., emb_dropout = 0.):
-        super().__init__()
-        assert image_size % patch_size == 0 , 'Image dimensions must be divisible by the patch size.'
-        num_patches = image_size // patch_size
-        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
-        patch_dim = channels * patch_size
-        self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (l p)-> b l (p c)',p=patch_size),
-            nn.Linear(patch_dim, dim),
-        )
-
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-        self.dropout = nn.Dropout(emb_dropout)
-
-        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
-
-        self.pool = pool
-        self.to_latent = nn.Identity()
-
-        self.mlp_head = nn.Sequential(
-            nn.LayerNorm(dim),
-            nn.Linear(dim, num_classes)
-        )
-
-    def forward(self, img):
-        #print(img.size())
-        x = self.to_patch_embedding(img)
-        b, n, _ = x.shape
-
-        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
-        x = torch.cat((cls_tokens, x), dim=1)
-        x += self.pos_embedding[:, :(n + 1)]
-        x = self.dropout(x)
-
-        x = self.transformer(x)
-
-        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
-
-        x = self.to_latent(x)
-        return self.mlp_head(x)
-
-v = myViT(
+v = MyViT.ViT( #定义ViT模型
     image_size = 2048,
     patch_size = 64,
     num_classes = 4,
@@ -120,13 +68,19 @@ v = myViT(
     dropout = 0.1,
     emb_dropout = 0.1
 ).to(device)#这里的训练强度已经减小了
+epochs = 2 #定义训练轮数
+
+# 加载模型
+# v=torch.load('./result/ViT-pretrained-net.pt')
 
 # Initialize the loss function
 loss_fn = torch.nn.CrossEntropyLoss()
 
-optimizer = torch.optim.Adam(v.parameters(), lr=learning_rate)
-ExpLR = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.8)
+learning_rate = 0.0008 #定义学习率
+optimizer = torch.optim.Adam(v.parameters(), lr=learning_rate) #定义优化器
+ExpLR = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.8) #绑定衰减学习率到优化器
 
+# 定义训练循环
 def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
@@ -142,8 +96,7 @@ def train_loop(dataloader, model, loss_fn, optimizer):
         if batch % 5 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-
+# 定义测试循环
 def test_loop(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -162,7 +115,7 @@ def test_loop(dataloader, model, loss_fn):
 
 last_loss=100
 now_loss=100
-for t in range(epochs):
+for t in range(epochs): # 开始训练
     new_lr=ExpLR.get_last_lr()[0]
     print(f"Epoch {t+1}\n-------------------------------")
     print(f'lr: {new_lr:>7e}')
@@ -177,3 +130,14 @@ for t in range(epochs):
     if last_loss/now_loss <1:
         ExpLR.step()
 print("Done!")
+
+torch.save(v, './result/ViT-pretrained-net.pt') # 保存训练的模型
+
+
+# 显示参数数量
+nb_param = 0
+for param in v.parameters():
+    nb_param += np.prod(list(param.data.size()))
+for param in v.parameters():
+    print(type(param.data), param.size())
+print('Number of parameters:', nb_param)
