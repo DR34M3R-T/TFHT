b75a571ab00c15474b514c969bcf1b041db3417a
DreamerT <1731428261@qq.com>
2022-05-08 10:49:39 +0800

    data collector

--- data_collect.py
+++ data_collect.py
@@ -1,11 +1,12 @@
 import sys
 import imp
-x=10
+x=5
+'''
 import comparemethods
 for i in range(x):
     #start
     output = sys.stdout
-    outputfile = open('result/pics/CNN1D/test{}.txt'.format(i+1),'w+')
+    outputfile = open('pics/CNN1D/test{}.txt'.format(i+1),'w+')
     sys.stdout = outputfile
     imp.reload(comparemethods)
     #end
@@ -17,11 +18,37 @@ import ViT_torch_train_rotor
 for i in range(x):
     #start
     output = sys.stdout
-    outputfile = open('result/pics/ViT/test{}.txt'.format(i+1),'w+')
+    outputfile = open('pics/ViT/test{}.txt'.format(i+1),'w+')
     sys.stdout = outputfile
     imp.reload(ViT_torch_train_rotor)
     #end
     outputfile.close()
     sys.stdout = output
     print("ViT {} of {}".format(i+1,x))
+'''
 
+'''
+from old import ViT_torch_train_rotor_without_FFT
+for i in range(x):
+    #start
+    output = sys.stdout
+    outputfile = open('pics/ViT_without_FFT/test{}.txt'.format(i+1),'w+')
+    sys.stdout = outputfile
+    imp.reload(ViT_torch_train_rotor_without_FFT)
+    #end
+    outputfile.close()
+    sys.stdout = output
+    print("ViT {} of {}".format(i+1,x))
+'''
+
+from old import ViT_torch_train_rotor_only_Freq
+for i in range(x):
+    #start
+    output = sys.stdout
+    outputfile = open('pics/ViT_only_Freq/test{}.txt'.format(i+1),'w+')
+    sys.stdout = outputfile
+    imp.reload(ViT_torch_train_rotor_only_Freq)
+    #end
+    outputfile.close()
+    sys.stdout = output
+    print("ViT {} of {}".format(i+1,x))
