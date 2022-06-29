43ce6138e1dbda5fc572a45c41f6c0153c51579e
DreamerT <1731428261@qq.com>
2022-06-29 14:30:35 +0800

    ViT_CWRU

--- ViT_torch_train_rotor_CWRU.py
+++ ViT_torch_train_rotor_CWRU.py
@@ -3,7 +3,7 @@ torch.set_default_tensor_type(torch.DoubleTensor)
 from torch.utils.data import DataLoader,Dataset
 from torchvision import transforms
 import numpy as np
-import MyViT
+import MyViT_CWRU
 
 
 # 设定训练用的设备
@@ -12,10 +12,14 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using {} device".format(device))
 
 # 导入raw数据集
-data_train = torch.from_numpy(np.load('./dataset/CWRU/data.npy'))
-label_train = torch.from_numpy(np.load('./dataset/CWRU/relabel.npy'))
-data_test = torch.from_numpy(np.load('./dataset/CWRU/data.npy'))
-lable_test = torch.from_numpy(np.load('./dataset/CWRU/relabel.npy'))
+data = torch.from_numpy(np.load('./dataset/CWRU/data.npy')) #7253 3 2048
+label = torch.from_numpy(np.load('./dataset/CWRU/relabel.npy')) #7253 7
+rand_arr = np.random.randint(low=0, high=9, size=(7253))
+rand_arr = torch.from_numpy(np.bool_(np.clip(rand_arr,2,3)-2))
+data_train = data[rand_arr==1]
+label_train = label[rand_arr==1]
+data_test = data[rand_arr==0]
+lable_test = label[rand_arr==0]
 
 # 自定义dataset和数据集预处理
 preprocess = transforms.Compose([
@@ -24,6 +28,7 @@ preprocess = transforms.Compose([
 ])
 class BearFaultDataset(Dataset):
     def __init__(self, inputs, targets, transform, reshape):
+        inputs =torch.squeeze(torch.split(inputs,1,1)[1],1)
         inputs_f=torch.abs(torch.fft.fft(inputs))
         #inputs_f/=len(inputs_f[0])/2
         #inputs_f[0]/=2
@@ -35,7 +40,10 @@ class BearFaultDataset(Dataset):
             self.inputs = self.inputs[:, :2025].reshape((-1, 45, 45))
         else:
             self.inputs = torch.cat((torch.unsqueeze(inputs,1),torch.unsqueeze(inputs_f,1)),1)
-        self.targets = targets
+        target_tmp = torch.split(targets,1,1)
+        self.targets_position = torch.clip(torch.squeeze(target_tmp[0],1)*3+torch.squeeze(target_tmp[1],1)-3,0)
+        self.targets = self.targets_position
+        values = torch.unique(self.targets)
         self.transform = transform
 
     def __len__(self):
@@ -54,17 +62,17 @@ training_data = BearFaultDataset(data_train, label_train, transform=preprocess, 
 test_data = BearFaultDataset(data_test, lable_test, transform=preprocess, reshape=isreshape)
 # print(training_data.inputs.shape, test_data.inputs.shape)
 # 定义dataloader
-batch_size = 64
+batch_size = 80
 train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
 test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
-v = MyViT.ViT( #定义ViT模型
+v = MyViT_CWRU.ViT( #定义ViT模型
     image_size = 2048,
     patch_size = 64,
-    num_classes = 4,
+    num_classes = 7,
     dim = 64,
-    depth = 2,
-    heads = 4,
+    depth = 3,
+    heads = 6,
     mlp_dim = 128,
     dropout = 0.1,
     emb_dropout = 0.1
@@ -77,14 +85,16 @@ epochs = 10 #定义训练轮数
 # Initialize the loss function
 loss_fn = torch.nn.CrossEntropyLoss()
 
-learning_rate = 0.0008 #定义学习率
-optimizer = torch.optim.Adam(v.parameters(), lr=learning_rate) #定义优化器
-ExpLR = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.8) #绑定衰减学习率到优化器
+learning_rate = 0.002 #定义学习率
+optimizer = torch.optim.NAdam(v.parameters(), lr=learning_rate,weight_decay=4e-5,momentum_decay=9e-4) #定义优化器
+ExpLR = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95) #绑定衰减学习率到优化器
 
 # 定义训练循环
 def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
+        X = X.to(device)
+        y = y.to(device)
         # Compute prediction and loss
         pred = model(X)
         loss = loss_fn(pred, y)
@@ -105,6 +115,8 @@ def test_loop(dataloader, model, loss_fn):
 
     with torch.no_grad():
         for X, y in dataloader:
+            X = X.to(device)
+            y = y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
@@ -124,12 +136,18 @@ for t in range(epochs): # 开始训练
     last_loss=now_loss
     now_loss=test_loop(test_dataloader, v, loss_fn)
     # 学习率动态衰减
-    if last_loss/now_loss <0.7:
+    if last_loss/now_loss <0.8:
         ExpLR.step()
-    if last_loss/now_loss <0.85:
         ExpLR.step()
-    if last_loss/now_loss <1:
+    if last_loss/now_loss <0.9:
         ExpLR.step()
+        ExpLR.step()
+    if last_loss/now_loss <1.0:
+        ExpLR.step()
+        ExpLR.step()
+    if last_loss/now_loss <1.4:
+        ExpLR.step()
+    else:ExpLR.step()
 print("Done!")
 
 torch.save(v.state_dict(), './result/ViT-state.pt') # 保存训练的模型
