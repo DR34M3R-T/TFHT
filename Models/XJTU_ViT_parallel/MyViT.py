19271591981d65f312b9eeb33381e994c0b86f10
DreamerT <1731428261@qq.com>
2022-07-21 17:03:24 +0800

    add parallel input method

---
+++ Models/XJTU_ViT_parallel/MyViT.py
@@ -0,0 +1,57 @@
+from sys import path_hooks
+import torch
+from torch import nn
+from vit_pytorch import Transformer
+from einops import repeat
+from einops.layers.torch import Rearrange
+
+#ViT
+class ViT(nn.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', path_num = 2, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super().__init__()
+        assert image_size % patch_size == 0 , 'Image dimensions must be divisible by the patch size.'
+        num_patches = image_size // patch_size
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+        patch_dim = patch_size
+        self.path_num = path_num
+        self.paralleler = nn.Sequential(
+            Rearrange('b c l-> c b l'),
+        )
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b (l p)-> b l p',p=patch_size),
+            nn.Linear(patch_dim, dim),
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+        self.to_latent = nn.Identity()
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim*path_num),
+            nn.Linear(dim*path_num, num_classes)
+        )
+
+    def forward(self, img):
+        #print(img.size())
+        img = self.paralleler(img)
+        x=['',''];
+        for i in range(self.path_num):
+            x[i] = self.to_patch_embedding(img[i])
+            b, n, _ = x[i].shape
+
+            cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
+            x[i] = torch.cat((cls_tokens, x[i]), dim=1)
+            x[i] += self.pos_embedding[:, :(n + 1)]
+            x[i] = self.dropout(x[i])
+
+            x[i] = self.transformer(x[i])
+
+            x[i] = x[i].mean(dim = 1) if self.pool == 'mean' else x[i][:, 0]
+        x_total = torch.cat((x[0],x[1]),dim=1)
+        x_total = self.to_latent(x_total)
+        x_total = self.mlp_head(x_total)
+        return x_total
