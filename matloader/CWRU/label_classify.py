2e963a7b8afb69839a9c793314e37aa4e2c8bb17
DreamerT <1731428261@qq.com>
2022-08-02 16:09:34 +0800

    little fix

--- matloader/CWRU/label_classify.py
+++ matloader/CWRU/label_classify.py
@@ -22,41 +22,52 @@ no_BA = no_BA[:,6]+\
     1000000000*no_BA[:,0]
 no_BA = np.unique(no_BA)'''
 
-relabel = np.zeros(label.shape[0],np.int32)-1
 #故障位置 4类
+relabel = np.zeros(label.shape[0],np.int32)-1
 relabel[label[:,0]==0]=0 #normal
 relabel[label[:,1]==1]=1 #B
 relabel[label[:,1]==2]=2 #IR
 relabel[label[:,1]==3]=3 #OR
+relabel[np.logical_and(label[:,2]==28,label[:,1]==1)]=101 #DE: B28
+relabel[np.logical_and(label[:,2]==28,label[:,1]==2)]=102 #DE: IR28
+np.save('dataset/CWRU/p4.npy',relabel)
 
 #故障端+故障位置 7类
+relabel = np.zeros(label.shape[0],np.int32)-1
 relabel[label[:,0]==0]=0 #normal
-relabel[label[:,0]==1&label[:,1]==1]=1 #DE: B IR OR
-relabel[label[:,0]==1&label[:,1]==2]=2 
-relabel[label[:,0]==1&label[:,1]==3]=3
-relabel[label[:,0]==2&label[:,1]==3]=4 #FE: B IR OR
-relabel[label[:,0]==2&label[:,1]==3]=5
-relabel[label[:,0]==2&label[:,1]==3]=6
+relabel[np.logical_and(label[:,0]==1,label[:,1]==1)]=1 #DE: B IR OR
+relabel[np.logical_and(label[:,0]==1,label[:,1]==2)]=2 
+relabel[np.logical_and(label[:,0]==1,label[:,1]==3)]=3
+relabel[np.logical_and(label[:,0]==2,label[:,1]==1)]=4 #FE: B IR OR
+relabel[np.logical_and(label[:,0]==2,label[:,1]==2)]=5
+relabel[np.logical_and(label[:,0]==2,label[:,1]==3)]=6
+relabel[np.logical_and(label[:,2]==28,label[:,1]==1)]=101 #DE: B28
+relabel[np.logical_and(label[:,2]==28,label[:,1]==2)]=102 #DE: IR28
+np.save('dataset/CWRU/e&p7.npy',relabel)
 
 #故障端+故障直径 7类
+relabel = np.zeros(label.shape[0],np.int32)-1
 relabel[label[:,0]==0]=0 #normal
-relabel[label[:,0]==1&label[:,2]==1]=1 #DE: 7 14 21
-relabel[label[:,0]==1&label[:,2]==2]=2 
-relabel[label[:,0]==1&label[:,2]==3]=3
-relabel[label[:,0]==2&label[:,2]==3]=4 #FE: 7 14 21
-relabel[label[:,0]==2&label[:,2]==3]=5
-relabel[label[:,0]==2&label[:,2]==3]=6
+relabel[np.logical_and(label[:,0]==1,label[:,2]==7)]=1 #DE: 7 14 21
+relabel[np.logical_and(label[:,0]==1,label[:,2]==14)]=2 
+relabel[np.logical_and(label[:,0]==1,label[:,2]==21)]=3
+relabel[np.logical_and(label[:,0]==2,label[:,2]==7)]=4 #FE: 7 14 21
+relabel[np.logical_and(label[:,0]==2,label[:,2]==14)]=5
+relabel[np.logical_and(label[:,0]==2,label[:,2]==21)]=6
+np.save('dataset/CWRU/e&d7.npy',relabel)
 
 #故障位置+故障直径 10类 
+relabel = np.zeros(label.shape[0],np.int32)-1
 relabel[label[:,0]==0]=0 #normal
-relabel[label[:,1]==1&label[:,2]==1]=1 #B: 7 14 21
-relabel[label[:,1]==1&label[:,2]==2]=2 
-relabel[label[:,1]==1&label[:,2]==3]=3
-relabel[label[:,1]==2&label[:,2]==3]=4 #IR: 7 14 21
-relabel[label[:,1]==2&label[:,2]==3]=5
-relabel[label[:,1]==2&label[:,2]==3]=6
-relabel[label[:,1]==3&label[:,2]==3]=7 #OR: 7 14 21
-relabel[label[:,1]==3&label[:,2]==3]=8
-relabel[label[:,1]==3&label[:,2]==3]=9
+relabel[np.logical_and(label[:,1]==1,label[:,2]==7)]=1 #B: 7 14 21
+relabel[np.logical_and(label[:,1]==1,label[:,2]==14)]=2 
+relabel[np.logical_and(label[:,1]==1,label[:,2]==21)]=3
+relabel[np.logical_and(label[:,1]==2,label[:,2]==7)]=4 #IR: 7 14 21
+relabel[np.logical_and(label[:,1]==2,label[:,2]==14)]=5
+relabel[np.logical_and(label[:,1]==2,label[:,2]==21)]=6
+relabel[np.logical_and(label[:,1]==3,label[:,2]==7)]=7 #OR: 7 14 21
+relabel[np.logical_and(label[:,1]==3,label[:,2]==14)]=8
+relabel[np.logical_and(label[:,1]==3,label[:,2]==21)]=9
+np.save('dataset/CWRU/p&d10.npy',relabel)
 
 pass
